73b9f82b31810cd83c577803dc1001909fa3e076
Wesley <wesley.larlarb@rookielabs.com>
2018-05-03 15:33:23 -0700

    Added Drivin fer realz

--- src/org/usfirst/frc/team5026/util/MotorGroup.java
+++ src/org/usfirst/frc/team5026/util/MotorGroup.java
@@ -15,8 +15,6 @@ public class MotorGroup implements SpeedController {
 	private TalonSRX motor1; 
 	/** Second motor */
 	private TalonSRX motor2;
-	/** Third motor */
-	private TalonSRX motor3;
 	/** Motors in an array */
 	private TalonSRX[] motors;
 
@@ -27,17 +25,14 @@ public class MotorGroup implements SpeedController {
 	 * @param motor2
 	 * @param motor3
 	 */
-	public MotorGroup(TalonSRX motor1, TalonSRX motor2, TalonSRX motor3) {
+	public MotorGroup(TalonSRX motor1, TalonSRX motor2) {
 		this.motor1 = motor1;
 		this.motor2 = motor2;
-		this.motor3 = motor3;
 		motors = new TalonSRX[3];
 		motors[0] = this.motor1;
 		motors[1] = this.motor2;
-		motors[2] = this.motor3;
 		this.motor2.follow(motor1);
-		this.motor3.follow(motor1);
-		setUp(motor1, motor2, motor3);
+		setUp(motor1, motor2);
 	}
 	
 	/**
